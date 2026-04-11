b701c97e9c270867a7ab2333978bf94413949b0d
adityapandey0027 <adityapandey8349@gmail.com>
2026-04-11 14:34:53 +0530

    update profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import { isAuth } from "../middlewares/isAuthMiddleware.js";
 import { createUserAddress, deleteUserAddress, deleteUserProfile, getUserAddress, getUserProfile, updateUserAddress, updateUserProfile } from "../controllers/userController.js";
 import { addCartItem, clearCart, getCartItem, removeCartItem } from "../controllers/cartController.js";
+import upload from '../middlewares/uploadMiddleware.js';
 
 const userRoutes = express.Router();
 
 userRoutes.get("/profile", isAuth, getUserProfile);
-userRoutes.put("/profile", isAuth, updateUserProfile);
+userRoutes.put("/profile", isAuth, upload.single("image"), updateUserProfile);
 userRoutes.delete("/profile", isAuth, deleteUserProfile);
 
 // cart routes
